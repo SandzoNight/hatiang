e1a70b3b66f507357f3cc7249837ddf82fa3c0fa
SandzoNight <flukemdc@gmail.com>
2021-07-16 03:08:35 +0700

    Format code

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,14 @@ import sources from '../sources.json'
 export default function Footer() {
     return (
         <div>
-        <div className='text-center'>Open source on <a href='https://github.com/SandzoNight/hatiang' className='font-bold'>github</a></div>
-        <div className='text-center'>รวบรวมข้อมูลจาก</div>
-        <div className='text-center text-xs'>อัปเดตเมื่อ {sources.updated_at}</div>
-        <ul className='text-center'>
-          {sources.list.map(source => (
-            <li>{source}</li>
-          ))}
-        </ul>
-      </div>
+            <div className='text-center'>Open source on <a href='https://github.com/SandzoNight/hatiang' className='font-bold'>github</a></div>
+            <div className='text-center'>รวบรวมข้อมูลจาก</div>
+            <div className='text-center text-xs'>อัปเดตเมื่อ {sources.updated_at}</div>
+            <ul className='text-center'>
+            {sources.list.map(source => (
+                <li>{source}</li>
+            ))}
+            </ul>
+        </div>
     )
 }
