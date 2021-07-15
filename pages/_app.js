f3e38ce475f5f9b96602982e20bd7d5011b68cff
SandzoNight <flukemdc@gmail.com>
2021-07-16 02:49:20 +0700

    Improve SEO

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <title>ช่องทางหาเตียงผู้ป่วยโควิด-19</title>
+      <meta name='keywords' content="หาเตียง, หาเตียงโควิด, เบอร์หาเตียง, เว็บหาเตียง, เว็บหาเตียงโควิด, หาเตียง โควิด"></meta>
+      <meta name='description' content="รวบรวมช่องทางการติดต่อหาเตียงรักษาผู้ป่วยโควิด-19"></meta>
       <meta property='og:title' content='ช่องทางหาเตียงผู้ป่วยโควิด-19'/>
       <meta property='og:image' content='/static/og.png' />
       <meta property='og:description' content='รวบรวมช่องทางการติดต่อหาเตียงรักษาผู้ป่วยโควิด-19' />
