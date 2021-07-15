79c01bbafa101b1786dd16ed9ae87e183fb3f5ec
Pacharapol <fluke.pacharapol@maqe.com>
2021-07-15 21:42:21 +0700

    Add link to sourcecode repository

--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,7 @@ export default function Home() {
             </table>
           </div>
           <div>
+            <div className='text-center'>Open source on <a href='https://github.com/SandzoNight/hatiang' className='font-bold'>github</a></div>
             <div className='text-center'>รวบรวมข้อมูลจาก</div>
             <div className='text-center text-xs'>อัปเดตเมื่อ {sources.updated_at}</div>
             <ul className='text-center'>
