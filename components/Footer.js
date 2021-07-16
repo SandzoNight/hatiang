979c87479cff919958a28c7c80151621c46a1fe0
Pacharapol <fluke.pacharapol@maqe.com>
2021-07-16 21:24:12 +0700

    Remove collect source section from footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,12 +4,11 @@ export default function Footer() {
     return (
         <div>
             <div className='text-center'>Open source on <a href='https://github.com/SandzoNight/hatiang' className='font-bold'>github</a></div>
-            <div className='text-center'>รวบรวมข้อมูลจาก</div>
             <div className='text-center text-xs'>อัปเดตเมื่อ {sources.updated_at}</div>
             <ul className='text-center'>
-            {sources.list.map(source => (
-                <li>{source}</li>
-            ))}
+                {sources.list.map(source => (
+                    <li>{source}</li>
+                ))}
             </ul>
         </div>
     )
